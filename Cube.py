a2982ef992f8433c273c3b3689d679930fdbbc09
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-06-08 23:36:43 +0530

    Assignments

---
+++ Cube.py
@@ -0,0 +1,3 @@
+n=int(input())
+for i in range(1,n+1):
+    print("The current Number is: ", i ,"and the cube is ",i*i*i)
--- Loop.py
+++ Loop.py
@@ -1,3 +1,2 @@
-for i in range(-10,1):
-    if i==0: break
+for i in range(-10,0):
     print(i)
